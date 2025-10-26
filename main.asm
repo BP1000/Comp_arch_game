adc0ab1cbde3e36231cc9ae3523dfad022cb019d
Bhavik Patel <Bhavik Patel@WIN-FJSVKLLO8LC>
2025-10-25 19:28:05 -0700

    Created Calculate color procedures and generate random number procedures

--- main.asm
+++ main.asm
@@ -29,27 +29,150 @@ gameOverMSG BYTE "Game Over! the numbers were: ", 0
 playAgainMSG BYTE "Play again? (1=Yes, 0=No): ", 0
 
 ;Color Constants
-BLUE = 1
-GREEN = 2
-RED = 4
-INTENSE =8
-WHITE = 15
+DARK_BLUE = 1
+BLUE_ON_BLACK = 1
+GREEN_ON_BLACK = 2 
+RED_ON_BLACK = 4
+DARK_RED = 9
+INTENSE_BLUE = 9
+INTENSE_RED = 12
+WHITE_ON_BLACK = 15
 
 .code
-main PROC
-	mov edx, OFFSET welcomeMSG ;printing welcome message
+;generate a random number between MIN_NUM and MAX_NUM
+GenerateRandomNumber PROC
+	mov eax, MAX_NUM
+	sub eax, MIN_NUM
+	inc eax
+	call RandomRange
+	add eax, MIN_NUM
+	ret
+GenerateRandomNumber ENDP
+
+;set up the game with new random numbers
+InitializeGame PROC
+	call Randomize
+	call GenerateRandomNumber
+	mov num1, eax
+	call GenerateRandomNumber
+	mov num2, eax
+	mov attempts, 0
+	mov gameWon, 0
+	ret
+InitializeGame ENDP
+
+; determines the color for the "Too High" meessage based on the distance between the user guess and the actual value
+CalculateColorHigh PROC
+	mov ebx, eax
+	mov eax, ebx
+	mov ecx, 100
+	mmul ecx,
+	mov ecx, MAX_NUM
+	div ecx ;stores the how out oof range the guess is as a percentage
+
+	cmp eax, 75
+	jg very_off
+	cmp eax, 50
+	jg off
+	cmp eax, 25
+	jg close
+	jmp really_close
+
+; set colors for very off guess
+very_off:
+	mov eax, DARK_RED
+	jmp done_color_high
+
+; set color for off guess
+off:
+	mov eax, RED_ON_BLACK
+	jmp done_color_high
+close:
+	mov eax, INTENSE_RED
+	jmp done_color_high
+really_close:
+	mov eax, WHITE_ON_BLACK
+	jmp done_color_high
+done_color_high:
+	ret
+CalculateColorHigh ENDP
+
+; determines the color for the "Too Low" meessage based on the distance between the user guess and the actual value
+CalculateColorLow PROC
+	mov ebx, eax
+	mov eax, ebx
+	mov ecx, 100
+	mul ecx
+	mov ecx, MAX_NUM
+	div ecx ;stores the how out of range the guess is as a percentage
+
+	cmp eax, 75
+	jg very_off_low
+	cmp eax, 50
+	jg far_low
+	cmp eax, 25
+	jg close_low
+	jmp close_low
+
+very_far_low:
+	mov eax, DARK_BLUE
+	jmp done_color_low
+far_low:
+	mov eax, BLUE_ON_BLACK
+	jmp done_color_low
+medium_low:
+	mov eax, INTENSE_BLUE
+	jmp done_color_low
+close_low:
+	mov eax, WHITE_ON_BLACK
+	jmp done_color_low
+color_done_low:
+	ret
+CalculateColorLow ENDP
+
+CheckGuess PROC
+	cmp eax, ebx
+	je correct_guess
+	jg too_high_guess
+	mov edx, OFFSET tooLowMSG
+	push eax
+	push ebx
+	mov eax, ebx
+	sub eax, [esp + 4]
+	call CalculateColorLow
+	call SetTextColor
+	pop ebx
+	pop eax
 	call WriteString
 	call Crlf
-	call Crlf
+	mov eax, WHITE_ON_BLACK
+	call SetTextColor
+	jmp check_done
 
-	mov edx, OFFSET instructions ;printing instructions
+too_high_guess:
+	mov edx, OFFSET tooHighMSG
+	push eax
+	push ebx
+	sub eax, ebx
+	call CalculateColorHigh
+	call SetTextColor
+	pop ebx
+	pop eax
 	call WriteString
 	call Crlf
+	mov eax, WHITE_ON_BLACK
+	call SetTextColor
+	jmp check_done
+correct_guess:
+	mov edx, OFFSET correctMSG
+	mov eax, GREEN_ON_BLACK
+	call SetTextColor
+	call WriteString
 	call Crlf
+	mov eax, WHITE_ON_BLACK
+	call SetTextColor
+check_done:
+	ret
+CheckGuess ENDP
 
 
-
-
-	INVOKE ExitProcess, 0
-main ENDP
-END main
